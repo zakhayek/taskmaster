c04733bfddc89f8689d997fc6720e3961e36d355
Zak Hayek <zakhayek1@gmail.com>
2021-03-17 14:57:25 -0700

    Initial commit. Project hierarchy setup.

---
+++ client/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div><h1>APP.</h1></div>
+    )
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env'],
+              ['@babel/preset-react'],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: { 
+    extensions: ['.js', '.jsx'],
+    modules: [path.resolve(__dirname, 'node_modules/tone'), 'node_modules'],
+  },
+  mode: 'development',
+};
